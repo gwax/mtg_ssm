1aaa2682cd98f4d8f8aafb772597017b816ec5d2
George Leslie-Waksman <waksman@gmail.com>
2015-07-12 18:39:23 -0700

    Forgot to test update_models.

--- tests/mtgjson_test.py
+++ tests/mtgjson_test.py
@@ -83,6 +83,7 @@ class MtgjsonTest(
         card = self.session.query(
             models.Card).filter_by(name='Snow-Covered Forest').first()
         self.assertEqual('Snow-Covered Forest', card.name)
+        self.assertEqual({'Land'}, {t.name for t in card.types})
         self.assertEqual({'Snow', 'Basic'}, {t.name for t in card.supertypes})
 
     def test_update_card(self):
@@ -101,8 +102,8 @@ class MtgjsonTest(
         card = self.session.query(
             models.Card).filter_by(name='Snow-Covered Forest').first()
         self.assertEqual('Snow-Covered Forest', card.name)
-        self.assertCountEqual(
-            ['Snow', 'Basic'], [t.name for t in card.supertypes])
+        self.assertEqual({'Land'}, {t.name for t in card.types})
+        self.assertEqual({'Snow', 'Basic'}, {t.name for t in card.supertypes})
 
     def test_create_printing(self):
         # Setup
@@ -162,8 +163,46 @@ class MtgjsonTest(
 
         # Execute
         mtgjson.update_printing(self.session, card_data, card, card_set)
+        self.session.commit()
 
         # Verify
         printings = self.session.query(models.CardPrinting).all()
         self.assertEqual(1, len(printings))
         self.assertEqual('Mark Zug', printings[0].artist.name)
+
+    def test_update_models(self):
+        # Execute
+        mtgjson.update_models(self.session, self.mtg_data)
+        self.session.commit()
+
+        # Verify
+        printings = self.session.query(models.CardPrinting).all()
+        set_card_mv_number = [
+            (p.set.code, p.card.name, p.multiverseid, p.set_number)
+            for p in printings]
+        expected = [
+            ('LEA', 'Air Elemental', 94, None),
+            ('LEA', 'Forest', 288, None),
+            ('LEA', 'Forest', 289, None),
+            ('ICE', 'Forest', 2746, None),
+            ('ICE', 'Forest', 2747, None),
+            ('ICE', 'Forest', 2748, None),
+            ('ICE', 'Snow-Covered Forest', 2749, None),
+            ('HML', 'Cemetery Gate', 2913, None),
+            ('HML', 'Cemetery Gate', 2914, None),
+            ('S00', 'Rhox', None, None),
+            ('pMGD', 'Black Sun\'s Zenith', None, '7'),
+            ('HOP', 'Academy at Tolaria West', 198073, '1'),
+            ('HOP', 'Akroma\'s Vengeance', 205366, '1'),
+            ('ARC', 'All in Good Time', 212648, '1'),
+            ('ARC', 'Leonin Abunas', 220527, '1'),
+            ('ISD', 'Abattoir Ghoul', 222911, '85'),
+            ('ISD', 'Delver of Secrets', 226749, '51a'),
+            ('ISD', 'Insectile Aberration', 226755, '51b'),
+            ('PC2', 'Akoum', 226512, '9'),
+            ('PC2', 'Armored Griffin', 271234, '1'),
+            ('PC2', 'Chaotic Æther', 226509, '1'),
+            ('PC2', 'Stairs to Infinity', 226521, 'P1'),
+            ('VMA', 'Academy Elite', 382835, '55'),
+        ]
+        self.assertCountEqual(expected, set_card_mv_number)
