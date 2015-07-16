00fa0b049ed335a719c7d5ebb79ca6132fcb9751
George Leslie-Waksman <waksman@gmail.com>
2015-07-16 16:44:11 -0700

    Clear warning about not being able to collect TestModel.

--- tests/util_test.py
+++ tests/util_test.py
@@ -14,7 +14,7 @@ class MyEnum(enum.IntEnum):
     one = 1
     two = 2
 
-class TestModel(sqld.declarative_base()):
+class Model(sqld.declarative_base()):
     __tablename__ = 'test'
 
     id = sqla.Column(sqla.Integer, primary_key=True)
@@ -26,17 +26,17 @@ class SqlEnumTest(sqlite_testcase.SqliteTestCase):
     def setUp(self):
         super().setUp()
         connection = self.engine.connect()
-        TestModel.metadata.create_all(connection)
+        Model.metadata.create_all(connection)
         connection.close()
 
     def test_store_retrieve(self):
         # Store
-        self.session.add(TestModel(id=1, enum_col=MyEnum.one))
-        self.session.add(TestModel(id=2, enum_col=MyEnum.two))
+        self.session.add(Model(id=1, enum_col=MyEnum.one))
+        self.session.add(Model(id=2, enum_col=MyEnum.two))
         self.session.commit()
 
         # Retrieve
-        objs = self.session.query(TestModel).all()
+        objs = self.session.query(Model).all()
         obj_dicts = [{'id': o.id, 'enum_col': o.enum_col} for o in objs]
         expected = [
             {'id': 1, 'enum_col': MyEnum.one},
@@ -46,8 +46,8 @@ class SqlEnumTest(sqlite_testcase.SqliteTestCase):
 
     def test_db_strings(self):
         # Setup
-        self.session.add(TestModel(id=1, enum_col=MyEnum.one))
-        self.session.add(TestModel(id=2, enum_col=MyEnum.two))
+        self.session.add(Model(id=1, enum_col=MyEnum.one))
+        self.session.add(Model(id=2, enum_col=MyEnum.two))
         self.session.commit()
 
         # Verify
