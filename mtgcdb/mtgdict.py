ab48b032afc0e73126d1706bd2cfc542ae3602f4
George Leslie-Waksman <waksman@gmail.com>
2015-09-08 10:05:20 -0700

    Performance optimization.

--- mtgcdb/mtgdict.py
+++ mtgcdb/mtgdict.py
@@ -1,47 +1,81 @@
 """Methods for managing data in the form of dicts."""
 
+import collections
+
 from mtgcdb import models
 
 
-def find_printing(session, card_dict):
-    """Given a card_dict, find the matching CardPrinting."""
-    mtgj_id = card_dict.get('id')
-    card_name = card_dict.get('name')
+def get_printing(
+        card_dict, id_to_prints, set_name_num_mv_to_prints,
+        set_name_mv_to_prints, set_name_num_to_prints):
+    """Given a card dict and various indexes, get the matching CardPrinting."""
+    mtgjsid = card_dict.get('id')
     set_code = card_dict.get('set')
-    set_number = card_dict.get('number')
-    multiverseid = card_dict.get('multiverseid')
+    name = card_dict.get('name')
+    mvid = card_dict.get('multiverseid')
+    number = card_dict.get('number')
 
-    mquery = session.query(models.CardPrinting)
+    if mtgjsid is not None and mtgjsid in id_to_prints:
+        return id_to_prints[mtgjsid]
 
-    if mtgj_id is not None:
-        return mquery.get(mtgj_id)
-
-    if card_name is None or set_code is None:
+    if set_code is None or name is None:
+        print('Card has no set or card has no name and cannot be found.')
         return None
-    mquery = mquery.filter_by(card_name=card_name, set_code=set_code)
 
-    if set_number is not None:
-        mquery = mquery.filter_by(set_number=set_number)
+    print('Could not find card by id, trying (set, name, num, mvid).')
+    set_name_num_mv = (set_code, name, number, mvid)
+    if set_name_num_mv in set_name_num_mv_to_prints:
+        prints = set_name_num_mv_to_prints[set_name_num_mv]
+        if len(prints) == 1:
+            return prints[0]
+        else:
+            print('{} entries found.'.format(len(prints)))
 
-    if multiverseid is not None:
-        mquery = mquery.filter_by(multiverseid=multiverseid)
+    print('trying (set, name, mv)')
+    set_name_mv = (set_code, name, mvid)
+    if set_name_mv in set_name_mv_to_prints:
+        prints = set_name_mv_to_prints[set_name_mv]
+        if len(prints) == 1:
+            return prints[0]
+        else:
+            print('{} entries found.'.format(len(prints)))
 
-    printings = mquery.all()
-    if not printings:
-        return None
-    elif len(printings) > 1:
-        print('Multiple matches found.')
-        return None
-    else:
-        return printings[0]
+    print('trying (set, name, number)')
+    set_name_num = (set_code, name, number)
+    if set_name_num in set_name_num:
+        prints = set_name_num_to_prints[set_name_num]
+        if len(prints) == 1:
+            return prints[0]
+        else:
+            print('{} entries found.'.format(len(prints)))
+
+    print('Warning: Could not find printing for {}'.format(card_dict))
+    return None
+
+
 
 
 def load_counts(session, card_dicts):
     """Load counts from dicts of card info/counts into the database."""
+    printings = session.query(models.CardPrinting)
+    id_to_prints = {}
+    set_name_num_mv_to_prints = collections.defaultdict(list)
+    set_name_mv_to_prints = collections.defaultdict(list)
+    set_name_num_to_prints = collections.defaultdict(list)
+    for printing in printings:
+        set_code = printing.set_code
+        name = printing.card_name
+        mvid = printing.multiverseid
+        num = printing.set_number
+        id_to_prints[printing.id] = printing
+        set_name_num_mv_to_prints[(set_code, name, num, mvid)].append(printing)
+        set_name_mv_to_prints[(set_code, name, mvid)].append(printing)
+        set_name_num_to_prints[(set_code, name, num)].append(printing)
+
     for card_dict in card_dicts:
-        printing = find_printing(session, card_dict)
-        if printing is None:
-            print('Warning: Could not find printing for {}'.format(card_dict))
+        printing = get_printing(
+            card_dict, id_to_prints, set_name_num_mv_to_prints,
+            set_name_mv_to_prints, set_name_num_to_prints)
 
         for key in models.CountTypes.__members__.keys():
             count = card_dict.get(key)
