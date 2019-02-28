4783c64579bd1ed25c349093f51000fcd99a110e
George Leslie-Waksman <waksman@gmail.com>
2019-02-27 22:11:34 -0800

    Fix indexes and add tests.

--- mtg_ssm/containers/indexes.py
+++ mtg_ssm/containers/indexes.py
@@ -3,7 +3,6 @@
 import collections
 from typing import Dict
 from typing import List
-from typing import Set
 from typing import Tuple
 from uuid import UUID
 
@@ -13,28 +12,16 @@ from mtg_ssm.scryfall.models import ScryCard
 from mtg_ssm.scryfall.models import ScrySet
 
 
-def name_card_sort_key(card: ScryCard) -> Tuple[str, int, str, Tuple[int, ...], UUID]:
+def name_card_sort_key(card: ScryCard) -> Tuple[str, int, str]:
     """Key function for sorting cards in a by-name list."""
     card_num, card_var = util.collector_int_var(card)
-    return (
-        card.set,
-        card_num or 0,
-        card_var or "",
-        tuple(card.multiverse_ids or (0,)),
-        card.id,
-    )
+    return (card.set, card_num or 0, card_var or "")  # TODO: sort by set release date
 
 
-def set_card_sort_key(card: ScryCard) -> Tuple[int, str, Tuple[int, ...], str, UUID]:
+def set_card_sort_key(card: ScryCard) -> Tuple[int, str]:
     """Key function for sorting cards in a by-set list."""
     card_num, card_var = util.collector_int_var(card)
-    return (
-        card_num or 0,
-        card_var or "",
-        tuple(card.multiverse_ids or (0,)),
-        card.name,
-        card.id,
-    )
+    return (card_num or 0, card_var or "")
 
 
 class ScryfallDataIndex:
@@ -50,29 +37,30 @@ class ScryfallDataIndex:
     def load_data(self, scrydata: ScryfallDataSet) -> None:
         """Load all cards and sets from a Scryfall data set."""
         self.id_to_card = {}
-        self.name_to_cards = {}
-        self.setcode_to_cards = {}
         self.setcode_to_id_to_index = {}
         self.setcode_to_set = {}
 
-        name_to_unsorted_cards: Dict[str, Set[ScryCard]] = collections.defaultdict(set)
-        setcode_to_unsorted_cards: Dict[str, Set[ScryCard]] = collections.defaultdict(
-            set
+        name_to_unsorted_cards: Dict[str, List[ScryCard]] = collections.defaultdict(
+            list
+        )
+        setcode_to_unsorted_cards: Dict[str, List[ScryCard]] = collections.defaultdict(
+            list
         )
 
         for card in scrydata.cards:
             self.id_to_card[card.id] = card
-            name_to_unsorted_cards[card.name].add(card)
-            setcode_to_unsorted_cards[card.set].add(card)
+            name_to_unsorted_cards[card.name].append(card)
+            setcode_to_unsorted_cards[card.set].append(card)
         for set_ in scrydata.sets:
             self.setcode_to_set[set_.code] = set_
 
-        for name, unsorted_cards in name_to_unsorted_cards.items():
-            self.name_to_cards[name] = sorted(unsorted_cards, key=name_card_sort_key)
+        for cards_list in name_to_unsorted_cards.values():
+            cards_list.sort(key=name_card_sort_key)
+        self.name_to_cards = dict(name_to_unsorted_cards)
 
-        for setcode, unsorted_cards in setcode_to_unsorted_cards.items():
-            sorted_cards = sorted(unsorted_cards, key=set_card_sort_key)
-            self.setcode_to_cards[setcode] = sorted_cards
+        for setcode, cards_list in setcode_to_unsorted_cards.items():
+            cards_list.sort(key=set_card_sort_key)
             self.setcode_to_id_to_index[setcode] = {
-                c.id: i for i, c in enumerate(sorted_cards)
+                c.id: i for i, c in enumerate(cards_list)
             }
+        self.setcode_to_cards = dict(setcode_to_unsorted_cards)
--- tests/containers/test_indexes.py
+++ tests/containers/test_indexes.py
@@ -1,5 +1,7 @@
 """Tests for mtg_ssm.containers.indexes."""
 
+from uuid import UUID
+
 from mtg_ssm.containers.bundles import ScryfallDataSet
 from mtg_ssm.containers.indexes import ScryfallDataIndex
 
@@ -7,3 +9,83 @@ from mtg_ssm.containers.indexes import ScryfallDataIndex
 def test_load_data(scryfall_data: ScryfallDataSet) -> None:
     index = ScryfallDataIndex()
     index.load_data(scryfall_data)
+
+
+def test_id_to_card(scryfall_data: ScryfallDataSet) -> None:
+    index = ScryfallDataIndex()
+    index.load_data(scryfall_data)
+    assert (
+        index.id_to_card[UUID("59cf0906-04fa-4b30-a7a6-3d117931154f")].name
+        == "Abattoir Ghoul"
+    )
+
+
+def test_name_to_cards(scryfall_data: ScryfallDataSet) -> None:
+    index = ScryfallDataIndex()
+    index.load_data(scryfall_data)
+
+    # set sorting
+    assert [c.set for c in index.name_to_cards["Dark Ritual"]] == ["hop", "ice", "lea"]
+
+    # collector number sorting
+    assert [(c.set, c.collector_number) for c in index.name_to_cards["Forest"]] == [
+        ("ice", "380"),
+        ("ice", "381"),
+        ("ice", "382"),
+        ("isd", "262"),
+        ("isd", "263"),
+        ("isd", "264"),
+        ("lea", "294"),
+        ("lea", "295"),
+    ]
+
+    # collector number variant sorting
+    assert [(c.set, c.collector_number) for c in index.name_to_cards["Thallid"]] == [
+        ("fem", "74a"),
+        ("fem", "74b"),
+        ("fem", "74c"),
+        ("fem", "74d"),
+        ("mma", "167"),
+    ]
+
+
+def test_setcode_to_cards(scryfall_data: ScryfallDataSet) -> None:
+    index = ScryfallDataIndex()
+    index.load_data(scryfall_data)
+
+    # collector number sorting
+    assert [(c.name, c.collector_number) for c in index.setcode_to_cards["isd"]] == [
+        ("Delver of Secrets // Insectile Aberration", "51"),
+        ("Abattoir Ghoul", "85"),
+        ("Forest", "262"),
+        ("Forest", "263"),
+        ("Forest", "264"),
+    ]
+
+    # collector number variant sorting
+    assert [c.collector_number for c in index.setcode_to_cards["fem"]] == [
+        "74a",
+        "74b",
+        "74c",
+        "74d",
+    ]
+
+
+def test_setcode_to_id_to_index(scryfall_data: ScryfallDataSet) -> None:
+    index = ScryfallDataIndex()
+    index.load_data(scryfall_data)
+
+    assert index.setcode_to_id_to_index["isd"] == {
+        UUID("11bf83bb-c95b-4b4f-9a56-ce7a1816307a"): 0,
+        UUID("59cf0906-04fa-4b30-a7a6-3d117931154f"): 1,
+        UUID("b606f644-1728-4cb3-90ed-121838875de1"): 2,
+        UUID("16f52885-1f01-4f06-90a8-1a0ecf291ab5"): 3,
+        UUID("4dea3762-c6ae-4304-aee4-6c3f56685319"): 4,
+    }
+
+
+def test_setcode_to_set(scryfall_data: ScryfallDataSet) -> None:
+    index = ScryfallDataIndex()
+    index.load_data(scryfall_data)
+
+    assert index.setcode_to_set["isd"].name == "Innistrad"
