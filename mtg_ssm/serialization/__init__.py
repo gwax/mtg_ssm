17883c8d14da50cc7e5129fa3dcacda9120c414d
George Leslie-Waksman <waksman@gmail.com>
2019-03-01 00:27:06 -0800

    Test csv

--- mtg_ssm/serialization/__init__.py
+++ mtg_ssm/serialization/__init__.py
@@ -1,5 +1,6 @@
 """Ensure that all serializers are imported to properly set up interface."""
+# import mtg_ssm.serialization.deckbox
 import mtg_ssm.serialization.csv
-import mtg_ssm.serialization.deckbox
 import mtg_ssm.serialization.interface
-import mtg_ssm.serialization.xlsx
+
+# import mtg_ssm.serialization.xlsx
--- tests/serialization/test_csv.py
+++ tests/serialization/test_csv.py
@@ -1,135 +1,153 @@
 """Tests for mtg_ssm.serialization.csv."""
 # pylint: disable=redefined-outer-name
 
-import tempfile
+from pathlib import Path
 import textwrap
+from typing import Dict
+from uuid import UUID
 
-import pytest
-
-from mtg_ssm.mtg import card_db
-from mtg_ssm.mtg import counts
+from mtg_ssm.containers import counts
+from mtg_ssm.containers.bundles import ScryfallDataSet
+from mtg_ssm.containers.collection import MagicCollection
+from mtg_ssm.containers.counts import ScryfallCardCount
+from mtg_ssm.containers.indexes import Oracle
+from mtg_ssm.scryfall.models import ScryCard
 from mtg_ssm.serialization import csv
 
-TEST_PRINT_ID = "fc46a4b72d216117a352f59217a84d0baeaaacb7"
+TEST_CARD_ID = UUID("57f25ead-b3ec-4c40-972d-d750ed2f5319")
 
 
-@pytest.fixture
-def cdb(sets_data):
-    """card_db fixture for testing."""
-    sets_data = {k: v for k, v in sets_data.items() if k in {"MMA", "pMGD"}}
-    return card_db.CardDb(sets_data)
-
-
-def test_header():
+def test_header() -> None:
     assert csv.CSV_HEADER == [
         "set",
         "name",
-        "number",
-        "multiverseid",
-        "id",
-        "copies",
-        "foils",
+        "collector_number",
+        "scryfall_id",
+        "nonfoil",
+        "foil",
     ]
 
 
-def test_row_for_printing(cdb):
-    printing = cdb.id_to_printing[TEST_PRINT_ID]
-    print_counts = {counts.CountTypes.copies: 3, counts.CountTypes.foils: 5}
-    csv_row = csv.row_for_printing(printing, print_counts)
+def test_row_for_card(id_to_card: Dict[UUID, ScryCard]) -> None:
+    card = id_to_card[TEST_CARD_ID]
+    card_counts = {counts.CountType.nonfoil: 3, counts.CountType.foil: 5}
+    csv_row = csv.row_for_card(card, card_counts)
     assert csv_row == {
-        "set": "MMA",
-        "name": "Thallid",
-        "number": "167",
-        "multiverseid": 370352,
-        "id": "fc46a4b72d216117a352f59217a84d0baeaaacb7",
-        "copies": 3,
-        "foils": 5,
+        "set": "phop",
+        "name": "Stairs to Infinity",
+        "collector_number": "P1",
+        "scryfall_id": TEST_CARD_ID,
+        "nonfoil": 3,
+        "foil": 5,
     }
 
 
-def test_rows_for_printings_verbose(cdb):
-    print_counts = {TEST_PRINT_ID: {counts.CountTypes.copies: 3}}
-    rows = csv.rows_for_printings(cdb, print_counts, True)
+def test_rows_for_cards_verbose(
+    scryfall_data: ScryfallDataSet, id_to_card: Dict[UUID, ScryCard]
+) -> None:
+    cards = [
+        id_to_card[TEST_CARD_ID],
+        id_to_card[UUID("dd88131a-2811-4a1f-bb9a-c82e12c1493b")],
+    ]
+    card_counts: ScryfallCardCount = {TEST_CARD_ID: {counts.CountType.nonfoil: 3}}
+    collection = MagicCollection(
+        oracle=Oracle(ScryfallDataSet(cards=cards, sets=scryfall_data.sets)),
+        counts=card_counts,
+    )
+    rows = csv.rows_for_cards(collection, True)
     assert list(rows) == [
         {
-            "set": "pMGD",
-            "name": "Black Sun's Zenith",
-            "number": "7",
-            "multiverseid": None,
-            "id": "6c9ffa9ffd2cf7e6f85c6be1713ee0c546b9f8fc",
+            "set": "phop",
+            "name": "Stairs to Infinity",
+            "collector_number": "P1",
+            "scryfall_id": TEST_CARD_ID,
+            "nonfoil": 3,
         },
         {
-            "set": "MMA",
-            "name": "Thallid",
-            "number": "167",
-            "multiverseid": 370352,
-            "id": "fc46a4b72d216117a352f59217a84d0baeaaacb7",
-            "copies": 3,
+            "set": "pmbs",
+            "name": "Black Sun's Zenith",
+            "collector_number": "39",
+            "scryfall_id": UUID("dd88131a-2811-4a1f-bb9a-c82e12c1493b"),
         },
     ]
 
 
-def test_rows_for_printings_terse(cdb):
-    print_counts = {TEST_PRINT_ID: {counts.CountTypes.copies: 3}}
-    rows = csv.rows_for_printings(cdb, print_counts, False)
+def test_rows_for_cards_terse(scryfall_data: ScryfallDataSet) -> None:
+    card_counts: counts.ScryfallCardCount = {
+        TEST_CARD_ID: {counts.CountType.nonfoil: 3}
+    }
+    collection = MagicCollection(oracle=Oracle(scryfall_data), counts=card_counts)
+    rows = csv.rows_for_cards(collection, False)
     assert list(rows) == [
         {
-            "set": "MMA",
-            "name": "Thallid",
-            "number": "167",
-            "multiverseid": 370352,
-            "id": "fc46a4b72d216117a352f59217a84d0baeaaacb7",
-            "copies": 3,
+            "set": "phop",
+            "name": "Stairs to Infinity",
+            "collector_number": "P1",
+            "scryfall_id": TEST_CARD_ID,
+            "nonfoil": 3,
         }
     ]
 
 
-def test_write_verbose(cdb):
-    print_counts = {
-        TEST_PRINT_ID: {counts.CountTypes.copies: 1, counts.CountTypes.foils: 12}
+def test_write_verbose(
+    scryfall_data: ScryfallDataSet, id_to_card: Dict[UUID, ScryCard], tmp_path: Path
+) -> None:
+    csv_path = tmp_path / "outfile.csv"
+    cards = [
+        id_to_card[TEST_CARD_ID],
+        id_to_card[UUID("dd88131a-2811-4a1f-bb9a-c82e12c1493b")],
+    ]
+    card_counts: ScryfallCardCount = {
+        TEST_CARD_ID: {counts.CountType.nonfoil: 3, counts.CountType.foil: 7}
     }
-    serializer = csv.CsvFullDialect(cdb)
-    with tempfile.NamedTemporaryFile(mode="rt") as outfile:
-        serializer.write(outfile.name, print_counts)
-        csvdata = outfile.read()
-    assert csvdata == textwrap.dedent(
-        """\
-        set,name,number,multiverseid,id,copies,foils
-        pMGD,Black Sun\'s Zenith,7,,6c9ffa9ffd2cf7e6f85c6be1713ee0c546b9f8fc,,
-        MMA,Thallid,167,370352,fc46a4b72d216117a352f59217a84d0baeaaacb7,1,12
-        """
+    collection = MagicCollection(
+        oracle=Oracle(ScryfallDataSet(cards=cards, sets=scryfall_data.sets)),
+        counts=card_counts,
     )
+    serializer = csv.CsvFullDialect()
+    serializer.write(csv_path, collection)
+    with csv_path.open("rt") as csv_file:
+        assert csv_file.read() == textwrap.dedent(
+            """\
+            set,name,collector_number,scryfall_id,nonfoil,foil
+            phop,Stairs to Infinity,P1,57f25ead-b3ec-4c40-972d-d750ed2f5319,3,7
+            pmbs,Black Sun\'s Zenith,39,dd88131a-2811-4a1f-bb9a-c82e12c1493b,,
+            """
+        )
 
 
-def test_write_terse(cdb):
-    print_counts = {
-        TEST_PRINT_ID: {counts.CountTypes.copies: 1, counts.CountTypes.foils: 12}
+def test_write_terse(scryfall_data: ScryfallDataSet, tmp_path: Path) -> None:
+    csv_path = tmp_path / "outfile.csv"
+    card_counts: counts.ScryfallCardCount = {
+        TEST_CARD_ID: {counts.CountType.nonfoil: 3}
     }
-    serializer = csv.CsvTerseDialect(cdb)
-    with tempfile.NamedTemporaryFile(mode="rt") as outfile:
-        serializer.write(outfile.name, print_counts)
-        csvdata = outfile.read()
-    assert csvdata == textwrap.dedent(
-        """\
-        set,name,number,multiverseid,id,copies,foils
-        MMA,Thallid,167,370352,fc46a4b72d216117a352f59217a84d0baeaaacb7,1,12
-        """
-    )
+    collection = MagicCollection(oracle=Oracle(scryfall_data), counts=card_counts)
+
+    serializer = csv.CsvTerseDialect()
+    serializer.write(csv_path, collection)
+    with csv_path.open("rt") as csv_file:
+        assert csv_file.read() == textwrap.dedent(
+            """\
+            set,name,collector_number,scryfall_id,nonfoil,foil
+            phop,Stairs to Infinity,P1,57f25ead-b3ec-4c40-972d-d750ed2f5319,3,
+            """
+        )
 
 
-def test_read(cdb):
-    with tempfile.NamedTemporaryFile("w") as infile:
-        infile.write(
+def test_read(scryfall_data: ScryfallDataSet, tmp_path: Path) -> None:
+    csv_path = tmp_path / "infile.csv"
+    oracle = Oracle(scryfall_data)
+    with csv_path.open("wt") as csv_file:
+        csv_file.write(
             textwrap.dedent(
                 """\
-                set,name,number,multiverseid,id,copies,foils
-                MMA,Thallid,167,370352,fc46a4b72d216117a352f59217a84d0baeaaacb7,3,72
+                set,name,collector_number,scryfall_id,nonfoil,foil
+                phop,Stairs to Infinity,P1,57f25ead-b3ec-4c40-972d-d750ed2f5319,3,7
                 """
             )
         )
-        infile.flush()
-        serializer = csv.CsvFullDialect(cdb)
-        print_counts = serializer.read(infile.name)
-    assert print_counts == {
-        TEST_PRINT_ID: {counts.CountTypes.copies: 3, counts.CountTypes.foils: 72}
+    serializer = csv.CsvFullDialect()
+    collection = serializer.read(csv_path, oracle)
+    assert collection.counts == {
+        TEST_CARD_ID: {counts.CountType.nonfoil: 3, counts.CountType.foil: 7}
     }
