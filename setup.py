b9de8d73ba7beffcc3d1c3b707af8d3cd3551cd4
George Leslie-Waksman <waksman@gmail.com>
2019-03-05 00:17:41 -0800

    Drop 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Topic :: Games/Entertainment",
